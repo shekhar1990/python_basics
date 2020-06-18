1ddd63d9eeb2fa3ce3e807361cde9f04706b7574
Shekhar Maurya <60063674+shekhar1990@users.noreply.github.com>
2020-06-18 18:59:39 +0530

    Add files via upload

---
+++ age_cal.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 20:42:27 2020
+
+@author: Shekhar.Maurya
+"""
+
+my_age=21
+after_5_years = my_age+5
+ten_years_back = my_age-10
+print("My age today", my_age)
+print("My age after 5 years", after_5_years)
+print("My age 10 years back",ten_years_back)
---
+++ bmi_cal.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 22:59:39 2020
+
+@author: Shekhar.Maurya
+"""
+
+weight = 80
+height = 1.72
+bmi = weight/height**2
+print("Body mass index",bmi)
